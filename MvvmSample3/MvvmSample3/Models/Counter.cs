1cceb852ca80a1ee8ce1e6ac78031c0b606cdb14
ykageyama <ykageyama@stcinc.co.jp>
2020-03-05 10:31:07 +0900

    プロジェクト ファイルを追加します。

---
+++ MvvmSample3/MvvmSample3/Models/Counter.cs
@@ -0,0 +1,36 @@
+﻿using Prism.Mvvm;
+
+namespace MvvmSample3.Models
+{
+    /// <summary>
+    /// カウンター
+    /// </summary>
+    class Counter : BindableBase
+    {
+        // プロパティ
+        // ★モデルのプロパティにはReactivePropertyを使わない
+        private int number;
+        public int Number
+        {
+            get { return number; }
+            // setは外部からできないようにする
+            private set { SetProperty(ref number, value); }
+        }
+
+        /// <summary>
+        /// Numberをデクリメント(-1)する
+        /// </summary>
+        public void Decrement()
+        {
+            Number -= 1;
+        }
+
+        /// <summary>
+        /// Numberをインクリメント(+1)する
+        /// </summary>
+        public void Increment()
+        {
+            Number += 1;
+        }
+    }
+}
---
+++ MvvmSample3/MvvmSample3/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,56 @@
+﻿using Prism.Navigation;
+using System;
+using MvvmSample3.Models;
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+using System.Reactive.Disposables;
+
+namespace MvvmSample3.ViewModels
+{
+    /// <summary>
+    /// MainPageのViewModel
+    /// </summary>
+    public class MainPageViewModel : ViewModelBase, IDisposable
+    {
+        // プロパティ
+        public ReactiveProperty<int> Number { get; } = new ReactiveProperty<int>();
+
+        // デクリメントコマンド
+        public ReactiveCommand DecrementCommand { get; } = new ReactiveCommand();
+
+        // インクリメントコマンド
+        public ReactiveCommand IncrementCommand { get; } = new ReactiveCommand();
+
+        // モデル
+        private Counter model = new Counter();
+
+        // オブジェクトをまとめてDisposeするためのモノ
+        // ★ReactivePropertyとModelのプロパティを同期するときのおまじない
+        private CompositeDisposable disposable = new CompositeDisposable();
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="navigationService"></param>
+        public MainPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            // NumberはModel(Counter)のNumberプロパティと同期する
+            // ★AddTo(disposable)はReactivePropertyとModelの同期をするときのおまじない
+            Number = model.ObserveProperty(x => x.Number).ToReactiveProperty().AddTo(disposable);
+            // デクリメントコマンド実行時の処理
+            DecrementCommand.Subscribe(() => model.Decrement());
+            // インクリメントコマンド実行時の処理
+            IncrementCommand.Subscribe(() => model.Increment());
+        }
+
+        /// <summary>
+        /// CompositeDisposableにためたオブジェクトをまとめてDisposeする
+        /// ★ReactivePropertyとModelのプロパティを同期するときのおまじない
+        /// </summary>
+        public void Dispose()
+        {
+            disposable.Dispose();
+        }
+    }
+}
