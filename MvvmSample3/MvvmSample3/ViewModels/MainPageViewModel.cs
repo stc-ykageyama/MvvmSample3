a8cf32dfc5bbb98e80528ca02c52f14b6d4d1d24
ykageyama <ykageyama@stcinc.co.jp>
2020-04-03 10:45:13 +0900

    オブジェクトの破棄をDestroyで行うよう修正

--- MvvmSample3/MvvmSample3/ViewModels/MainPageViewModel.cs
+++ MvvmSample3/MvvmSample3/ViewModels/MainPageViewModel.cs
@@ -4,13 +4,14 @@ using MvvmSample3.Models;
 using Reactive.Bindings;
 using Reactive.Bindings.Extensions;
 using System.Reactive.Disposables;
+using System.Threading.Tasks;
 
 namespace MvvmSample3.ViewModels
 {
     /// <summary>
     /// MainPageのViewModel
     /// </summary>
-    public class MainPageViewModel : ViewModelBase, IDisposable
+    public class MainPageViewModel : ViewModelBase
     {
         // ReactivePropetyオブジェクト、ReactiveCommandオブジェクトをまとめてDisposeするためのもの
         private CompositeDisposable Disposable { get; } = new CompositeDisposable();
@@ -19,10 +20,12 @@ namespace MvvmSample3.ViewModels
         public ReactiveProperty<int> Number { get; }
 
         // デクリメントコマンド
-        public ReactiveCommand DecrementCommand { get; } = new ReactiveCommand();
+        //public ReactiveCommand DecrementCommand { get; } = new ReactiveCommand();
+        public AsyncReactiveCommand DecrementCommand { get; } = new AsyncReactiveCommand();
 
         // インクリメントコマンド
-        public ReactiveCommand IncrementCommand { get; } = new ReactiveCommand();
+        //public ReactiveCommand IncrementCommand { get; } = new ReactiveCommand();
+        public AsyncReactiveCommand IncrementCommand { get; } = new AsyncReactiveCommand();
 
         // モデル
         private Counter model = new Counter();
@@ -39,15 +42,15 @@ namespace MvvmSample3.ViewModels
                 .ToReactiveProperty()                           // 監視対象のプロパティが変更されたらPropertyChangedイベントを発行する(ReactiveProperty化)
                 .AddTo(Disposable);                             // Dispose対象に追加する ※忘れないように!!
             // デクリメントコマンド実行時の処理
-            DecrementCommand.Subscribe(() => model.Decrement());
+            DecrementCommand.Subscribe(() => Task.Run(model.Decrement));
             // インクリメントコマンド実行時の処理
-            IncrementCommand.Subscribe(() => model.Increment());
+            IncrementCommand.Subscribe(() => Task.Run(model.Increment));
         }
 
         /// <summary>
         /// CompositeDisposableに追加したオブジェクトをまとめてDisposeする
         /// </summary>
-        public void Dispose()
+        public override void Destroy()
         {
             Disposable.Dispose();
         }
